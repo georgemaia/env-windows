070eebd9b8b224fac44bf36ab37a1948c6fcb047
George Maia <georgemaia@users.noreply.github.com>
2025-08-12 16:29:53 -0300

    Winget Apps install

--- winget.bat
+++ winget.bat
@@ -1,8 +1,14 @@
 REM Instalção de Pacotes utilizando o winget
 REM ----------------------------------------
+REM Git
 REM Laragon
 REM Microsfot Visual Studio Code
+REM Obsidian
+
+winget install Git.Git
 
 winget install LeNgocKhoa.Laragon 
 
-winget install Microsoft.VisualStudioCode 
+winget install Microsoft.VisualStudioCode
+
+winget install Obsidian.Obsidian
